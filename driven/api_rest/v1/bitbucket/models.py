b463686ac3540a8b14e29f4366ff6cfc059e4133
Fernando Salom <fer@rudo.es>
2025-03-13 14:28:30 +0100

    Add models for bitbucket adapter in driven side

---
+++ driven/api_rest/v1/bitbucket/models.py
@@ -0,0 +1,23 @@
+from typing import Optional, List
+
+from pydantic import BaseModel, HttpUrl
+
+
+class UserResponse(BaseModel):
+    display_name: str
+    type: str
+    uuid: str
+    account_id: str
+    nickname: str
+
+
+class WorkspaceMembershipResponse(BaseModel):
+    user: UserResponse
+
+
+class BitbucketResponse(BaseModel):
+    values: List[WorkspaceMembershipResponse]
+    pagelen: int
+    size: int
+    page: int
+    next: Optional[HttpUrl] = None
