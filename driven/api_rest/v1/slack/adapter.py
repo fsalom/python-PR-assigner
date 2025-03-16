5ec3ef526638f5ba1d7455e5b5333dc8dad4dc11
Fernando Salom <fer@rudo.es>
2025-03-16 08:26:30 +0100

    Add slack function to send message

--- driven/api_rest/v1/slack/adapter.py
+++ driven/api_rest/v1/slack/adapter.py
@@ -2,15 +2,11 @@ from dependency_injector.wiring import inject
 from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
+from application.ports.driven.slack_repository_port import SlackRepositoryPort
+
 slack_router = APIRouter()
 
 
-@slack_router.patch('/slack/me/device')
-@inject
-async def update_fcm(service: UserServicePort = Depends(Provide[UserContainer.service]),
-                     api_mapper: UserDTOMapper = Depends(Provide[UserContainer.api_mapper])):
-    await service.update_fcm_token(user, fcm_request.device_id, fcm_request.platform)
-    return JSONResponse(
-        status_code=status.HTTP_200_OK,
-        content=jsonable_encoder(api_mapper.to_domain())
-    )
+class SlackRepositoryAdapter(SlackRepositoryPort):
+    async def notify(self, user, message):
+        pass
