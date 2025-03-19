3d283d13528416474cd6647d98f9f2796ab943ee
Fernando Salom <fer@rudo.es>
2025-03-19 18:25:44 +0100

    Fix db model for pull request

--- driven/db/pr_per_user/models.py
+++ driven/db/pr_per_user/models.py
@@ -1,10 +1,8 @@
 from datetime import datetime
-
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
 
 class PullRequestPerUserMO(Base):
     __tablename__ = "pr_per_user"
@@ -12,9 +10,10 @@ class PullRequestPerUserMO(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(nullable=False)
     url: Mapped[str] = mapped_column(nullable=False)
-    created_at: Mapped[datetime] = mapped_column(auto_now_add=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow, nullable=False)
     created_by: Mapped[str] = mapped_column(nullable=False)
     assigned_to_email: Mapped[str] = mapped_column(nullable=False)
 
-    class Meta:
-        ordering = ('created_at',)
+    @classmethod
+    def order_by_created_at(cls, query):
+        return query.order_by(cls.created_at)
