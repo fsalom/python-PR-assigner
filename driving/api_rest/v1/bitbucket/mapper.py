f29d305911895c496a3ba1afab1d4ef107d74f79
Fernando Salom <fer@rudo.es>
2025-03-21 22:28:25 +0100

    Add new mapper for bitbucket api

--- driving/api_rest/v1/bitbucket/mapper.py
+++ driving/api_rest/v1/bitbucket/mapper.py
@@ -1,8 +1,27 @@
+from domain.pull_request import PullRequest
+from domain.repository import Repository
+from domain.user import User
 from domain.webhook_payload import WebhookPayload
+from driving.api_rest.v1.bitbucket.models import WebhookPayloadRequest
 
 
 class BitbucketDTOMapper:
 
     @staticmethod
-    def to_domain() -> WebhookPayload:
-        return WebhookPayload()
+    def to_domain(payload: WebhookPayloadRequest) -> WebhookPayload:
+        repository = Repository(
+            name=payload.repository.name,
+            full_name=payload.repository.full_name,
+            uuid=payload.repository.uuid,
+            links=payload.repository.links,
+        )
+        pull_request = PullRequest(id=payload.pullrequest.id,
+                                   title=payload.pullrequest.title,
+                                   description=payload.pullrequest.description,
+                                   state=payload.pullrequest.state,
+                                   author=payload.pullrequest.author.account_id,)
+        return WebhookPayload(
+            repository=repository,
+            pull_request=pull_request,
+            event=payload.event
+        )
