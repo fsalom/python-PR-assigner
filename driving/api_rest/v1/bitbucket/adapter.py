a6df5e2212a78f2e542271fb9fa89eac8df7050d
Fernando Salom <fer@rudo.es>
2025-03-21 22:29:13 +0100

    Add new logic for bitbucket webhook

--- driving/api_rest/v1/bitbucket/adapter.py
+++ driving/api_rest/v1/bitbucket/adapter.py
@@ -1,19 +1,24 @@
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
-from fastapi.encoders import jsonable_encoder
 from starlette import status
 from starlette.responses import JSONResponse
 
+from application.ports.driving.bitbucket_service_port import BitbucketServicePort
 from driving.api_rest.v1.bitbucket.mapper import BitbucketDTOMapper
+from driving.api_rest.v1.bitbucket.models import WebhookPayloadRequest
 from infrastructure.di.bitbucket.container import BitbucketContainer
 
 bitbucket_router = APIRouter()
 
 
-@bitbucket_router.get('/bitbucket/me')
+@bitbucket_router.post('/webhook')
 @inject
-async def webhook(api_mapper: BitbucketDTOMapper = Depends(Provide[BitbucketContainer.api_mapper])):
+async def webhook(
+        payload_request: WebhookPayloadRequest,
+        service: BitbucketServicePort = Depends(Provide[BitbucketContainer.service]),
+        api_mapper: BitbucketDTOMapper = Depends(Provide[BitbucketContainer.api_mapper])):
+    payload = api_mapper.to_domain(payload_request)
+    service.assign(payload)
     return JSONResponse(
                 status_code=status.HTTP_200_OK,
-                content=jsonable_encoder(api_mapper.to_domain())
-            )
+                content={"result": "ok"})
