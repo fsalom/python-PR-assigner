fede6e1217e254f0aa99860468e7c1d106cb8723
Fernando Salom <fer@rudo.es>
2025-03-21 22:29:26 +0100

    Fix naming

--- domain/webhook_payload.py
+++ domain/webhook_payload.py
@@ -1,12 +1,10 @@
 from pydantic import BaseModel
 
-from domain.actor import Actor
 from domain.pull_request import PullRequest
 from domain.repository import Repository
 
 
 class WebhookPayload(BaseModel):
-    actor: Actor
     repository: Repository
-    pullrequest: PullRequest
+    pull_request: PullRequest
     event: str
